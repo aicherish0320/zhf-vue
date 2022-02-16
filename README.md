698574894b02cc924f608e56404830c76d80725d
aicherish <aicherish0320@163.com>
2022-02-16 22:01:37 +0800

    feat: 添加“rollup配置”笔记

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 珠峰架构-VueJS
-手写 VueJS：数据响应式原理、依赖收集、虚拟DOM、diff算法
+
+手写 VueJS：数据响应式原理、依赖收集、虚拟 DOM、diff 算法
+
+## rollup 的配置
+
+使用 `Rollup` 搭建开发环境 1.什么是**Rollup**?
+`Rollup`是一个`JavaScript`模块打包器，可以将小块代码编译成大块复杂的代码，`rollup.js`更专注于`Javascript`类库打包（开发应用时使用`webpack`，开发库时使用`Rollup`）
+
+- `@babel/core`： babel 核心模块
+- `@babel/preset-env`：将高级语法转化成低级语法
+- `rollup-plugin-babel`：rollup 中使用 babel 插件
--- aic-vue/rollup.config.js
+++ aic-vue/rollup.config.js
@@ -2,12 +2,12 @@ import babel from 'rollup-plugin-babel'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 export default {
-  input: './src/index.js',
+  input: './src/index.js', // 打包入口
   output: {
     file: 'dist/vue.js',
-    format: 'umd',
+    format: 'umd', // 输出格式 (IIFE/ESM/CJS)
     name: 'Vue', // umd 模块需要配置 name，会将导出的模块放到 window 上
-    sourcemap: true
+    sourcemap: true // 可以进行源代码调试
   },
   plugins: [
     babel({
