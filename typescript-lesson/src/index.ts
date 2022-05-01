6ec45900134f0318e6bbdf223efc4fbe092d440a
aicherish <aicherish0320@163.com>
2022-05-02 07:49:47 +0800

    ts 中的类型断言

--- typescript-lesson/src/index.ts
+++ typescript-lesson/src/index.ts
@@ -1,35 +1,20 @@
-//! null undefined
-// ts 中 null 和 undefined 可以赋值给任何类型（任何类型的子类型）
-// 严格模式中不能将 null 和 undefined 赋值给给任何类型
-// nuLl -> null undefined -> undefined
+//! 联合类型 默认是并集的关系
 
-// const num: number = null
-const un: undefined = undefined
-const nu: null = null
-// ! void 空类型 一般用于函数的返回值类型
-function fn1(): void {}
-function fn2(): void {
-  return undefined
-}
-// function fn3(): void {
-//   // 严格模式下 不行
-//   return null
+const elm = document.getElementById('app')
+// '!'标识非空断言
+elm!.style.color = 'red'
+// if (elm) {
+//   elm.style.color = 'red'
 // }
 
-//! never 标识永远不
-// 1. 程序无法到终点，死循环，抛错；2. 判断的时候，出现 never；3. 用 never 来做一些特殊的处理
-function fn3(): never {
-  throw new Error()
-}
-function fn4(): never {
-  while (true) {}
-}
+// ! 类型断言
 
-// ! Symbol bigInt
+const el = elm as HTMLElement
+el.style.color = 'red'
 
-// ! object
-// ! any
+// ! 字面量类型
+type COLOR = 'red' | 'yellow' | 'blue'
 
-const s1 = Symbol()
+const color: COLOR = 'blue'
 
 export {}
